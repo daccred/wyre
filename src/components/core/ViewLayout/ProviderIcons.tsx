e04e526d28c45815c800575670029b787ca8d2e1
abahwisdom <abahwisdom@gmail.com>
2023-01-27 13:55:16 +0100

    admin page template design with updated icons

--- src/components/core/ViewLayout/ProviderIcons.tsx
+++ src/components/core/ViewLayout/ProviderIcons.tsx
@@ -194,13 +194,12 @@ export const ChevronDownIcon = (props: HTMLChakraProps<"svg">) => (
   width={25}
   height={25}
   viewBox="0 0 24 24"
-  fill="none"
+  stroke="#9F9F9F"
   xmlns="http://www.w3.org/2000/svg"
   {...props}
 >
   <path
     d="M6 9l6 6 6-6"
-    // stroke="#898787"
     strokeWidth={2}
     strokeLinecap="round"
     strokeLinejoin="round"
