743c19efdfceb8acc55167af9da8b011b02e5b01
abahwisdom <abahwisdom@gmail.com>
2023-01-31 00:05:09 +0100

    employee first page done with table and pagination

--- src/views/Employees/index.tsx
+++ src/views/Employees/index.tsx
@@ -8,47 +8,73 @@ import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
   Avatar,
   Flex, 
 } from "@chakra-ui/react";
 import ViewLayout from "../../components/core/ViewLayout";
-import { FiSearch } from 'react-icons/fi'
+import { FiSearch, FiArrowRight, FiArrowLeft } from 'react-icons/fi'
 import { PlusIcon } from "./ProviderIcons";
+import {
+  Pagination,
+  usePagination,
+  PaginationPage,
+  PaginationNext,
+  PaginationPrevious,
+  PaginationPageGroup,
+  PaginationContainer,
+  PaginationSeparator
+} from "@ajna/pagination";
+import { useEffect, useState } from "react";
 
 const Employees = () => {
 
-  const dummyData= [
-    {
-      fullName:'john doe',
-      category:'employee',
-      jobRole:'fullstack developer',
-      department:'engineering',
-      status:'active',
-      imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-    },
-    {
-      fullName:'john doe',
-      category:'employee',
-      jobRole:'fullstack developer',
-      department:'engineering',
-      status:'active',
-      imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-    },
-    {
-      fullName:'john doe',
-      category:'employee',
-      jobRole:'fullstack developer',
-      department:'engineering',
-      status:'active',
-      imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+  const [dummyData, setDummyData]= useState<{[key: string]: string}[]>([])
+
+  useEffect(()=>{
+    let v=[]
+    for(let i=0; i<24; i++){
+      v.push({
+        fullName:'john doe',
+        category:'employee',
+        jobRole:'fullstack developer',
+        department:'engineering',
+        status:'active',
+        imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+      })
     }
-  ]
+    setDummyData(v)
+  },[])
+
+  // const dummyData= [
+  //   {
+  //     fullName:'john doe',
+  //     category:'employee',
+  //     jobRole:'fullstack developer',
+  //     department:'engineering',
+  //     status:'active',
+  //     imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+  //   },
+  //   {
+  //     fullName:'john doe',
+  //     category:'employee',
+  //     jobRole:'fullstack developer',
+  //     department:'engineering',
+  //     status:'active',
+  //     imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+  //   },
+  //   {
+  //     fullName:'john doe',
+  //     category:'employee',
+  //     jobRole:'fullstack developer',
+  //     department:'engineering',
+  //     status:'active',
+  //     imgURL:"https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+  //   }
+  // ]
 
   const dummyEmployee={
     fullName:'john doe',
@@ -63,6 +89,39 @@ const Employees = () => {
     location:'Bay Area, California, USA',
     paymentMethod:'75% USD, 15% BTC, 10 ETH'
   }
+
+   // constants
+   const outerLimit = 2;
+   const innerLimit = 2;
+ 
+   const {
+     pages,
+     pagesCount,
+     offset,
+     currentPage,
+     setCurrentPage,
+    //  setIsDisabled,
+     isDisabled,
+     pageSize,
+    //  setPageSize
+   } = usePagination({
+     total: dummyData?.length,
+     limits: {
+       outer: outerLimit,
+       inner: innerLimit
+     },
+     initialState: {
+       pageSize: 10,
+       isDisabled: false,
+       currentPage: 1
+     }
+   });
+
+  // handlers
+  const handlePageChange = (nextPage: number): void => {
+    // -> request new data using the page number
+    setCurrentPage(nextPage);
+  };
 
   
   return (
@@ -88,7 +147,7 @@ const Employees = () => {
                 <Text  fontWeight={"semibold"} fontSize="sm">Active Employees</Text>
               </HStack>
             </HStack>
-            <TableContainer py="4"
+            <TableContainer pt="4"
             css={{
               "&::-webkit-scrollbar": {
                 width: "15px",
@@ -116,7 +175,7 @@ const Employees = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {dummyData && dummyData?.length>0 && dummyData?.map(data=>(
+                  {dummyData && dummyData?.length>0 && dummyData?.slice(((pageSize * currentPage)-pageSize),(pageSize * currentPage)).map(data=>(
                     <Tr textTransform={"capitalize"} >
                       <Td>
                         <HStack>
@@ -139,6 +198,75 @@ const Employees = () => {
                
               </Table>
             </TableContainer>
+            <Pagination
+              pagesCount={pagesCount}
+              currentPage={currentPage}
+              isDisabled={isDisabled}
+              onPageChange={handlePageChange}
+            >
+              <PaginationContainer
+                align="center"
+                justify="space-between"
+                py={2}
+                w="full"
+              >
+                <PaginationPrevious
+                  variant={"outline"}
+                  h="40px"
+                  px="12px"
+                  leftIcon={<FiArrowLeft/>}
+                  iconSpacing={3}
+                  border={"1px solid #D0D5DD"}
+                  borderRadius="8px"
+                  boxShadow={"0px 1px 2px rgba(16, 24, 40, 0.05)"}
+                >
+                  <Text>Previous</Text>
+                </PaginationPrevious>
+                <PaginationPageGroup
+                  isInline
+                  align="center"
+                  separator={
+                    <PaginationSeparator
+                      bg="#EAECF0"
+                      fontSize="sm"
+                      boxSize="10"
+                      jumpSize={11}
+                    />
+                  }
+                >
+                  {pages.map((page: number) => (
+                    <PaginationPage
+                      w={7}
+                      bg="white"
+                      key={`pagination_page_${page}`}
+                      page={page}
+                      fontSize="sm"
+                      boxSize="10"
+                      fontWeight='bold'
+                      _hover={{
+                        bg: "#EAECF0",
+                      }}
+                      _current={{
+                        bg: "#EAECF0",
+                        
+                      }}
+                    />
+                  ))}
+                </PaginationPageGroup>
+                <PaginationNext
+                variant={"outline"}
+                h="40px"
+                px="12px"
+                rightIcon={<FiArrowRight/>}
+                iconSpacing={3}
+                border={"1px solid #D0D5DD"}
+                borderRadius="8px"
+                boxShadow={"0px 1px 2px rgba(16, 24, 40, 0.05)"}
+                >
+                  <Text>Next</Text>
+                </PaginationNext>
+              </PaginationContainer>
+            </Pagination>
           </Stack>
           <Flex flexDirection={"column"} borderRadius={"15px"} border={"1px solid"} borderColor="bordergrey" p='4' bg={'white'} flex="1" marginInlineStart="0" >
             <Text fontWeight="bold" fontSize="18px" mb="4" >Employee Details</Text>
