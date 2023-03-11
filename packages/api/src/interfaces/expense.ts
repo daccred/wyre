6abf7e5f9db6a0efadd664a3c90fb21afc629cff
John Tawiah <johnbaktawiah@gmail.com>
2023-03-11 10:08:56 +0100

    imp(root): :art: Added a key to suspend payrolls, and also updated expenses to include expense type…

--- packages/api/src/interfaces/expense.ts
+++ packages/api/src/interfaces/expense.ts
@@ -3,6 +3,9 @@ import * as z from "zod";
 export const expenseSchema = z.object({
   amount: z.string(),
   description: z.string(),
+  date: z.date(),
+  type: z.enum(["Reimbursement", "Payment"]),
+  status: z.enum(["Pending", "Failed", "Success"]),
   employees: z.array(z.string()),
 });
 
