a0be5955198b5d4c86209acaef72703b08d25afd
Neon <daniel@tecmie.com>
2023-03-14 18:25:00 +0100

    can now create a new contractor

---
+++ packages/api/src/interfaces/contractor.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const contractorSchema = z.object({
+  email: z.string().email(),
+  department: z.string(),
+  jobRole: z.string(),
+  grossSalary: z.string(),
+  signingBonus: z.string(),
+  status: z.boolean(),
+  name: z.string(),
+  category: z.string(),
+});
+
+export type contractorSchemaType = z.infer<typeof contractorSchema>;
