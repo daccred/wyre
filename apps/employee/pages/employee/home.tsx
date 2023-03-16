3093d5071dc26f1f1502e2a70796700b2adcea73
Godsfavour Williams <godsfavour.solomon@tecmie.com>
2023-03-16 15:06:51 +0100

    build the Header

---
+++ apps/employee/pages/employee/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Meta } from "../../layouts";
+import View from "../../views/employee/Home";
+
+
+export default function Page() {
+
+  return (
+    <>
+      <Meta />
+      <View />
+    </>
+  );
+}
---
+++ apps/employee/pages/employee/request.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Meta } from "../../layouts";
+import View from "../../views/employee/Request";
+
+
+export default function Page() {
+
+  return (
+    <>
+      <Meta />
+      <View />
+    </>
+  );
+}
