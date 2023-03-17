10048a6291a3c7f4dab23eb792059001bc6b4114
fps <anthony.nnaemeka.umeh@gmail.com>
2023-03-17 11:20:53 +0100

    Provider interface.

---
+++ packages/database/prisma/migrations/20230313100517_ftotp/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "OTPs" (
+    "id" TEXT NOT NULL,
+    "otp" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "OTPs_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OTPs" ADD CONSTRAINT "OTPs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
